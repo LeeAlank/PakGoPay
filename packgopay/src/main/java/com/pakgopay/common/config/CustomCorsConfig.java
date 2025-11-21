0deadaede85da7b9e93877f4febd4a48906e13e9
zf <574676312@qq.com>
2025-11-21 14:35:27 +0800

    底座代码

---
+++ packgopay/src/main/java/com/pakgopay/common/config/CustomCorsConfig.java
@@ -0,0 +1,24 @@
+package com.pakgopay.common.config;
+
+import com.pakgopay.filter.PakGoPayInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+@EnableWebMvc
+public class CustomCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:5173");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration registration = registry.addInterceptor(new PakGoPayInterceptor());
+        registration.addPathPatterns("/**");
+    }
+}
